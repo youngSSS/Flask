596aa33330ab7a4028d63273a8b68574cd66a274
YoungSSS <young4127s@naver.com>
2021-02-08 22:35:17 +0900

    답변 등록 완성 및 기초 작업 완료

--- projects/flask-project/config.py
+++ projects/flask-project/config.py
@@ -4,6 +4,9 @@ BASE_DIR = os.path.dirname(__file__)
 
 
 # Database 접속 주소
-SQLALCHEMY_DATABASE_URI = "sqlite:///{}".format(os.path.join(BASE_DIR, "flyskys.db"))
+SQLALCHEMY_DATABASE_URI = "sqlite:///{}".format(
+    os.path.join(BASE_DIR, "flyskys.db"))
 # SQLAlchemy 이벤트 처리 옵션
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+# CSRF (Cross-site request forgery) 방지
+SECRET_KEY = "dev"
