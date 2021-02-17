71efbdb176267373b27c7597848b9bb73884f4b3
YoungSSS <young4127s@naver.com>
2021-02-17 21:34:05 +0900

    로그인 로그아웃 기능 구현

--- projects/flask-project/flyskys/views/auth_views.py
+++ projects/flask-project/flyskys/views/auth_views.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, url_for, render_template, flash, request
-from werkzeug.security import generate_password_hash
+from flask import Blueprint, url_for, render_template, flash, request, session, g
+from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
 
 from flyskys import db
-from flyskys.forms import UserCreateForm
+from flyskys.forms import UserCreateForm, UserLoginForm
 from flyskys.models import User
 
 
@@ -13,6 +13,8 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.route('/signup/', methods=('GET', 'POST'))
 def signup():
     form = UserCreateForm()
+
+    # Case: 최종 회원가입
     if request.method == 'POST' and form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if not user:
@@ -25,4 +27,44 @@ def signup():
         else:
             flash('이미 존재하는 사용자입니다')
 
+    # Case: 회원가입 bnt를 눌러 회원가입 창으로 이동하는 경우
     return render_template('auth/signup.html', form=form)
+
+
+@bp.route('/login/', methods=('GET', 'POST'))
+def login():
+    form = UserLoginForm()
+
+    # POST 요청에 대해서는 로그인 수행
+    if request.method == 'POST' and form.validate_on_submit():
+        error = None
+        user = User.query.filter_by(username=form.username.data).first()
+        if not user:
+            error = '존재하지 않는 사용자입니다.'
+        elif not check_password_hash(user.password, form.password.data):
+            error = '비밀번호가 올바르지 않습니다.'
+        if error is None:
+            # CASE: 사용자 존재 && 비밀번호 존재
+            # session에 키와 키값을 저장
+            session.clear()
+            session['user_id'] = user.id
+            return redirect(url_for('main.index'))
+        flash(error)
+
+    # GET 요청에 대해서는 로인인 템플릿 렌더링
+    return render_template('auth/login.html', form=form)
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.get(user_id)
+
+
+@bp.route('/logout/')
+def logout():
+    session.clear()
+    return redirect(url_for('main.index'))
