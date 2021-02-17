0fc8b7f7550a283d6061a1ded853b8ab88d7fb92
YoungSSS <young4127s@naver.com>
2021-02-17 21:53:26 +0900

    SQLite의 ORM 문제 해결 중

--- projects/flask-project/flyskys/__init__.py
+++ projects/flask-project/flyskys/__init__.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 
 import config
 
 
+naming_convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
 # 다른 모듈에서 db 객체를 호출하기 위해 global 선언
-db = SQLAlchemy()
+db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
 
 
@@ -16,7 +24,10 @@ def create_app():
 
     # ORM
     db.init_app(app)
-    migrate.init_app(app, db)
+    if app.config['SQLALCHEMY_DATABASE_URI'].startswith("sqlite"):
+        migrate.init_app(app, db, render_as_batch=True)
+    else:
+        migrate.init_app(app, db)
     from . import models
 
     # Blueprint
